880588c0a93f8652a6d57cf382dcc7d8c7091174
Kara Rehman <63965607+kararehman@users.noreply.github.com>
2026-05-06 09:37:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ document.querySelectorAll('.collapsedpreview').forEach(header => {
     header.closest('.projectcard').classList.toggle('open');
   });
 });
+
+//close all other open cards
+document.querySelectorAll('.projectcard.open').forEach(card => {
+      if (card !== clickedCard) {
+        card.classList.remove('open');
+      }
+    });
+
+ // toggle the clicked card
+    clickedCard.classList.toggle('open');
+  });
+});
